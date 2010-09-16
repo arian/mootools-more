7c27e4eff0e939290dd70c18d27bed69ce1f08b5
Arian <arian@aryweb.nl>
2010-09-16 15:14:33 +0200

    * Add Support for Filters in Validation

--- Source/Utilities/Validation.js
+++ Source/Utilities/Validation.js
@@ -26,15 +26,19 @@ var Validation = this.Validation = new Class({
 	Implements: Options,
 
 	options: {
-		allowEmpty: false
+		allowEmpty: false/*
+		filters: []*/
 	},
 
 	rules: [],
 	rulesArgs: [],
+	filters: [],
 
 	initialize: function(rules, options){
 		this.setOptions(options);
-		if (rules) this.addRules(rules);
+		if (rules) this.addRules(Array.from(rules));
+		if (this.options.filters) this.addFilters(this.options.filters);
+
 	},
 
 	addRule: function(rule){
@@ -50,12 +54,25 @@ var Validation = this.Validation = new Class({
 			this.addRule(rules[i]);
 	},
 
+	addFilter: function(filter){
+		if (Type.isString(filter)) filter = String.prototype[filter];
+		if (filter) this.filters.push(filter);
+	},
+
+	addFilters: function(filters){
+		for (var i = 0, l = filters.length; i < l; i++)
+			this.addFilter(filters[i]);
+	},
+
 	isValid: function(value, allowEmpty){
 		var errors = this.errors = [];
 
 		if (allowEmpty || (allowEmpty == null && this.options.allowEmpty)){
 			if (Validation.Rules['isEmpty'](value)) return true;
 		}
+
+		for (var i = 0, l = this.filters.length; i < l; i++)
+			value = this.filters[i].call(value, value);
 
 		var rules = this.rules,
 			result,
@@ -81,7 +98,6 @@ var Validation = this.Validation = new Class({
 		};
 		return this.getErrorCodes().map(fn);
 	}
-
 
 });
 
