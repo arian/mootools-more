d140057df857f94f58612f2c3b5689acfb37e1eb
Arian <arian@aryweb.nl>
2010-11-29 00:36:35 +0100

    The JSONSchema validator is using the https://github.com/kriszyp/json-schema version now
    - Easier API, just Validation.validate('jsonschema', value, schema);
    - Adds loads of Specs (for example from https://github.com/garycourt/JSV/blob/master/tests/tests3.js)

--- Source/Utilities/Validation.JSONSchema.js
+++ Source/Utilities/Validation.JSONSchema.js
@@ -22,19 +22,20 @@ provides: [Validation.JSONSchema]
 
 // Implemented according the http://tools.ietf.org/html/draft-zyp-json-schema-03
 // Implementation inspired by dojox.json.validation - https://github.com/dojo/dojox/blob/master/json/schema.js
+// and https://github.com/kriszyp/json-schema
 
 (function(){
 
-var validate = function(instance, options){
+var validate = function(instance, schema, _changing) {
 
-	if (!options) options = {};
-	var schema = options.schema,
-		_changing = options._changing,
-		errors = [];
+	var errors = [],
+		undefined;
 
+	// validate a value against a property definition
 	var checkProp = function(value, schema, path, i){
 		var l;
 		path += path ? ((typeof i == 'number') ? '[' + i + ']' : ((typeof i == 'undefined') ? '' : '.' + i)) : i;
+
 		var addError = function(message){
 			errors.push({
 				property: path,
@@ -42,9 +43,13 @@ var validate = function(instance, options){
 			});
 		};
 
-		if ((typeof schema != 'object' || schema instanceof Array) && (path || typeof schema != 'function')){
+		if (
+			(typeof schema != 'object' || schema instanceof Array) &&
+			(path || typeof schema != 'function') &&
+			!(schema && schema.type)
+		){
 			if (typeof schema == 'function'){
-				if (!(Object(value) instanceof schema)){
+				if (!(value instanceof schema)){
 					addError('is not an instance of the class/constructor ' + schema.name);
 				}
 			} else if (schema){
@@ -52,52 +57,54 @@ var validate = function(instance, options){
 			}
 			return null;
 		}
-		if (_changing && schema.readonly){
-			addError('is a readonly field, it can not be changed');
-		}
-		if (schema['extends']){ // if it extends another schema, it must pass that schema as well
-			checkProp(value,schema['extends'],path,i);
-		}
+
+		if (_changing && schema.readonly) addError('is a readonly field, it can not be changed');
+
+		// if it extends another schema, it must pass that schema as well
+		if (schema['extends']) checkProp(value,schema['extends'],path,i);
+
 		// validate a value against a type definition
-		var checkType = function(type,value){
-			if(type){
-				if (
-					typeof type == 'string' && type != 'any' &&
-					(type == 'null' ? value !== null : typeof value != type) &&
-					!(value instanceof Array && type == 'array') &&
-					!(type == 'integer' && value % 1 === 0)
-				){
-					return [{
-						property: path,
-						message: (typeof value) + ' value found, but a ' + type + ' is required'
-					}];
-				}
-				if (type instanceof Array){
-					var unionErrors = [];
-					for (var j = 0, l = type.length; j < l; j++){ // a union type
-						if (!(unionErrors = checkType(type[j], value)).length) break;
-					}
-					if (unionErrors.length) return unionErrors;
-				} else if (typeof type == 'object'){
-					var priorErrors = errors;
-					errors = [];
-					checkProp(value,type,path);
-					var theseErrors = errors;
-					errors = priorErrors;
-					return theseErrors;
-				}
+		var checkType = function(type, value){
+			if (!type) return [];
+			if (
+				typeof type == 'string' &&
+				type != 'any' &&
+				(type == 'null' ? value !== null : typeof value != type) &&
+				!(value instanceof Array && type == 'array') &&
+				!(value instanceof Date && type == 'date') &&
+				!(type == 'integer' && value % 1 === 0)
+			){
+				return [{
+					property: path,
+					message: (typeof value) + ' value found, but a ' + type + ' is required'
+				}];
 			}
+
+			if (type instanceof Array){
+				var unionErrors = [];
+				for (var j = 0; j < type.length; j++){ // a union type
+					if (!(unionErrors = checkType(type[j], value)).length) break;
+				}
+				if (unionErrors.length) return unionErrors;
+			} else if (typeof type == 'object'){
+				var priorErrors = errors;
+				errors = [];
+				checkProp(value, type, path);
+				var theseErrors = errors;
+				errors = priorErrors;
+				return theseErrors;
+			}
+
 			return [];
-		}
+		};
 		if (value === undefined){
-			if (!schema.optional){
-				addError('is missing and it is not optional');
-			}
+			if (!schema.optional && !schema.get) addError('is missing and it is not optional');
 		} else {
-			errors = errors.concat(checkType(schema.type,value));
+			errors = errors.concat(checkType(schema.type, value));
 			if (schema.disallow && !checkType(schema.disallow, value).length){
 				addError(' disallowed value was matched');
 			}
+
 			if (value !== null){
 				if (value instanceof Array){
 					if (schema.items){
@@ -117,7 +124,7 @@ var validate = function(instance, options){
 					if (schema.maxItems && value.length > schema.maxItems){
 						addError('There must be a maximum of ' + schema.maxItems + ' in the array');
 					}
-				} else if(schema.properties){
+				} else if (schema.properties || schema.additionalProperties){
 					errors.concat(checkObj(value, schema.properties, path, schema.additionalProperties));
 				}
 				if (schema.pattern && typeof value == 'string' && !value.match(schema.pattern)){
@@ -129,20 +136,25 @@ var validate = function(instance, options){
 				if (schema.minLength && typeof value == 'string' && value.length < schema.minLength){
 					addError('must be at least ' + schema.minLength + ' characters long');
 				}
-				if (typeof schema.minimum !== undefined && typeof value == typeof schema.minimum &&
-						schema.minimum > value){
+				if (
+					typeof schema.minimum !== undefined &&
+					typeof value == typeof schema.minimum &&
+					schema.minimum > value
+				){
 					addError('must have a minimum value of ' + schema.minimum);
 				}
-				if (typeof schema.maximum !== undefined && typeof value == typeof schema.maximum &&
-						schema.maximum < value){
+				if (
+					typeof schema.maximum !== undefined &&
+					typeof value == typeof schema.maximum &&
+					schema.maximum < value
+				){
 					addError('must have a maximum value of ' + schema.maximum);
 				}
 				if (schema['enum']){
-					var enumer = schema['enum'];
+					var enumer = schema['enum'], found;
 					l = enumer.length;
-					var found;
-					for (var j = 0; j < l; j++) if(enumer[j]===value){
-						found=1;
+					for (var j = 0; j < l; j++) if (enumer[j] === value){
+						found = 1;
 						break;
 					}
 					if (!found) addError('does not have a value in the enumeration ' + enumer.join(', '));
@@ -156,14 +168,13 @@ var validate = function(instance, options){
 			}
 		}
 		return null;
-	}
+	};
+
 
 	// validate an object against a schema
 	var checkObj = function(instance, objTypeDef, path, additionalProp){
 
-		var i;
-
-		if (typeof objTypeDef =='object'){
+		if (typeof objTypeDef == 'object'){
 			if (typeof instance != 'object' || instance instanceof Array){
 				errors.push({
 					property: path,
@@ -171,38 +182,46 @@ var validate = function(instance, options){
 				});
 			}
 
-			for (i in objTypeDef){
-				if(objTypeDef.hasOwnProperty(i) && !(i.charAt(0) == '_' && i.charAt(1) == '_')){
+			for (var i in objTypeDef){
+				if (objTypeDef.hasOwnProperty(i) && !(i.charAt(0) == '_' && i.charAt(1) == '_')){
 					var value = instance[i];
 					var propDef = objTypeDef[i];
-					checkProp(value,propDef,path,i);
+					// set default
+					if (value === undefined && propDef['default']){
+						value = instance[i] = propDef['default'];
+					}
+					if (propDef.coerce && exports.coerce && i in instance){
+						value = instance[i] = exports.coerce(value, propDef);
+					}
+					checkProp(value, propDef, path, i);
 				}
 			}
 		}
 
-		for (i in instance){
+		for (var i in instance){
+
 			if (
 				instance.hasOwnProperty(i) &&
-				!(i.charAt(0) == '_' &&
-				i.charAt(1) == '_') &&
+				!(i.charAt(0) == '_' && i.charAt(1) == '_') &&
 				objTypeDef &&
 				!objTypeDef[i] &&
-				additionalProp===false
-			){
-				errors.push({
-					property: path,
-					message: (typeof value) + 'The property ' + i + ' is not defined in the schema and the schema does not allow additional properties'
-				});
-			}
-			var requires = objTypeDef && objTypeDef[i] && objTypeDef[i].requires;
-			if (requires && !(requires in instance)){
-				errors.push({
-					property: path,
-					message:'the presence of the property ' + i + ' requires that ' + requires + ' also be present'
-				});
-			}
+				additionalProp === false
+			) errors.push({
+				property: path,
+				message: (typeof value) + 'The property ' + i + ' is not defined in the schema and the schema does not allow additional properties'
+			});
+
+			var requires = (objTypeDef && objTypeDef[i] && objTypeDef[i].requires);
+			if (requires && !(requires in instance)) errors.push({
+				property: path,
+				message: 'the presence of the property ' + i + ' requires that ' + requires + ' also be present'
+			});
+
 			value = instance[i];
-			if (objTypeDef && typeof objTypeDef == 'object' && !(i in objTypeDef)){
+			if (additionalProp && (!(objTypeDef && typeof objTypeDef == 'object') || !(i in objTypeDef))){
+				if (additionalProp.coerce && exports.coerce){
+					value = instance[i] = exports.coerce(value, additionalProp);
+				}
 				checkProp(value, additionalProp, path, i);
 			}
 			if (!_changing && value && value.$schema){
@@ -210,25 +229,27 @@ var validate = function(instance, options){
 			}
 		}
 		return errors;
-	}
+	};
 
 	if (schema) checkProp(instance, schema, '', _changing || '');
-	if(!_changing && instance && instance.$schema) checkProp(instance, instance.$schema, '', '');
+	if (!_changing && instance && instance.$schema) checkProp(instance, instance.$schema, '', '');
 
-	return errors.length ? errors : true;
+	return {
+		valid: !errors.length,
+		errors: errors
+	};
 
 };
 
 
 Validation.defineRules({
 
-	jsonschema: function(instance, options){
-		return validate(instance, options);
+	jsonschema: function(instance, schema){
+		return validate(instance, schema, false);
 	},
 
-	jsonschemapropertychange: function(instance, options){
-		if (!options._changing) options._changing = 'property';
-		return validate(instance, options);
+	jsonschemapropertychange: function(instance, schema, property){
+		return validate(instance, schema, property || 'property');
 	}
 
 });
--- Specs/1.3/Utilities/Validation.JSONSchema.js
+++ Specs/1.3/Utilities/Validation.JSONSchema.js
@@ -1,3 +1,4 @@
+
 /*
 Script: Validation.js
 	Specs for Validation.js
@@ -6,7 +7,9 @@ License:
 	MIT-style license.
 */
 
+
 // Tests borrowed from https://github.com/dojo/dojox/blob/master/json/tests/schema.js
+// and https://github.com/garycourt/JSV/blob/master/tests/tests3.js
 
 describe('Validation.JSONSchema', function(){
 
@@ -282,27 +285,184 @@ describe('Validation.JSONSchema', function(){
 		}
 	};
 
-	it('tests a simple self-validating test', function(){
-		var validation = new Validation('jsonschema');
-		expect(validation.validate(simpleObj)).toBeTruthy();
+
+	// Even a shorter shortcut for Validation.validate
+	var val = function(){
+		return Validation.validate.apply(null, ['jsonschema'].concat(Array.from(arguments)));
+	};
+
+
+	describe('Is valid for schema', function(){
+
+		it('tests a simple self-validating test', function(){
+			expect(val(simpleObj)).toEqual(true);
+		});
+
+		it('tests bigger instance and scheme', function(){
+			var validation = new Validation().addRule('jsonschema', biggerSchema);
+			expect(validation.validate(biggerObj)).toEqual(true);
+
+			expect(validation.validate(invalidBiggerObj)).toEqual(false);
+			expect(validation.getErrors().length).toEqual(3);
+		});
+
+		xit('tests the schema for schemas against it\'s self. Narcissistic testing', function(){
+			console.log(JSON.validateSchema(schemaForSchemas, schemaForSchemas));
+			expect(JSON.validateSchema(schemaForSchemas, schemaForSchemas));
+			expect(val(schemaForSchemas, schemaForSchemas)).toEqual(true);
+		});
+
+		xit('tests the big schema against the schema for schemas', function(){
+			expect(val(biggerSchema, schemaForSchemas)).toEqual(true);
+		});
+
 	});
 
-	it('tests bigger instance and scheme', function(){
-		var validation = new Validation({name: 'jsonschema', args: {schema: biggerSchema}})
-		expect(validation.validate(biggerObj)).toBeTruthy();
-		expect(validation.validate(invalidBiggerObj)).toBeFalsy();
-		validation.getErrors(function(error){
-			var result = error.result;
-			expect(result.length).toEqual(3);
+	xdescribe('Is valid property change', function(){
+
+		it('should fail', function(){
+			var result = val('jsonschemapropertychange', biggerSchema, biggerObj);
+			expect(result).toEqual(false); // it returns an array instead of false
 		});
+
+		it('should be valid', function(){
+			var result = val('jsonschemapropertychange', schemaForSchemas, schemaForSchemas);
+			expect(result).toBeTruthy();
+		});
+
 	});
 
-	xit('tests the schema for schemas against it\'s self. Narcissitic testing', function(){
-		var validation = new Validation({
-			name: 'jsonschema',
-			args: {schema: schemaForSchemas}
-		});
-		expect(validation.validate(schemaForSchemas)).toBeTruthy();
+	it('tests primitive validation', function(){
+		expect(val({})).toEqual(true);
+		expect(val([])).toEqual(true);
+		expect(val('')).toEqual(true);
+		expect(val(00)).toEqual(true);
+		expect(val(false)).toEqual(true);
+		expect(val(null)).toEqual(true);
+	});
+
+	it('tests types validation', function(){
+		// simple types
+		expect(val({}, {type : 'object'})).toEqual(true);
+		expect(val([], {type : 'array'})).toEqual(true);
+		expect(val('', {type : 'string'})).toEqual(true);
+		expect(val(00, {type : 'number'})).toEqual(true);
+		expect(val(00, {type : 'integer'})).toEqual(true);
+		expect(val(false, {type : 'boolean'})).toEqual(true);
+		expect(val(null, {type : 'null'})).toEqual(true);
+		expect(val(true, {type : 'any'})).toEqual(true);
+
+		// expect(val(null, {type : 'object'})).toEqual(false);
+		expect(val(null, {type : 'array'})).toEqual(false);
+		expect(val(null, {type : 'string'})).toEqual(false);
+		expect(val(null, {type : 'number'})).toEqual(false);
+		expect(val(0.1, {type : 'integer'})).toEqual(false);
+		expect(val(null, {type : 'boolean'})).toEqual(false);
+		expect(val(false, {type : 'null'})).toEqual(false);
+
+		// union type
+		expect(val({}, {type : ['null', 'boolean', 'number', 'integer', 'string', 'array', 'object']})).toEqual(true);
+		expect(val({}, {type : ['null', 'boolean', 'number', 'integer', 'string', 'array']})).toEqual(false);
+
+		// schema union type
+		expect(val({}, {type : [{type : 'string'}, {type : 'object'}]})).toEqual(true);
+		expect(val(55, {type : [{type : 'string'}, {type : 'object'}, 'number']})).toEqual(true);
+		// expect(val([], {type : [{type : 'string'}, {type : 'object'}]})).toEqual(false);
+
+	});
+
+	it('tests the types in an array', function(){
+		var schema = {
+			items: [{type: 'string'}, {type: 'number'}]
+		};
+		expect(val(['moo', 300], schema)).toEqual(true);
+		expect(val([10, 20], schema)).toEqual(false);
+	});
+
+	it('tests the type of a property where multiple types are allowed', function(){
+		var schema = {
+			type: ['string', 'number']
+		};
+		expect(val('Moo!', schema)).toEqual(true);
+		expect(val(200, schema)).toEqual(true);
+		expect(val(null, schema)).toEqual(false);
+		expect(val({foo: 'bar'}, schema)).toEqual(false);
+	});
+
+	it('tests properties', function(){
+		expect(val({}, {type: 'object', properties: {}})).toEqual(true);
+		expect(val({a: 1}, {type: 'object', properties: {a: {}}})).toEqual(true);
+		expect(val({a: 1}, {type: 'object', properties: {a: {type: 'number'}}})).toEqual(true);
+		expect(val({a: {b :'two'}}, {type: 'object', properties: {a: {type: 'object', properties: {b: {type: 'string'}}}}})).toEqual(true);
+
+		expect(val({a: 1}, {type: 'object', properties: {a: {type: 'string'}}})).toEqual(false);
+		expect(val({a: {b: 'two'}}, {type: 'object', properties: {a: {type: 'object', properties: {b: {type : 'number'}}}}})).toEqual(false);
+	});
+
+	it('tests PatternProperties', function(){
+		expect(val(null, {patternProperties: {}})).toEqual(true);
+		expect(val({}, {patternProperties: {}})).toEqual(true);
+		expect(val({a: 1}, {patternProperties: {'[a-z]': {}}})).toEqual(true);
+		expect(val({a: 1, b: 2, cc: '3'}, {patternProperties: {'^[a-z]$': {type: 'number'}}})).toEqual(true);
+		expect(val({a: {b: 'two'}}, {patternProperties: {'[a-z]': {patternProperties: {'[a-z]': {type: 'string'}}}}})).toEqual(true);
+
+		// expect(val({a: 1, b: 2, c: '3' }, {patternProperties: {'^[a-z]$': {type: 'number'}}})).toEqual(false);
+		// expect(val({a: {b: 'two'}}, {patternProperties: {'[a-z]' : {patternProperties : {'[a-z]': {type: 'number'}}}}})).toEqual(false);
+	});
+
+	it('tests the disallow property', function(){
+		// Everytime the type of the value is not in the 'disallowed' array
+		expect(val({}, {disallow: ['null', 'boolean', 'number', 'integer', 'string', 'array']})).toBeTruthy();
+		// expect(val([], {disallow: ['null', 'boolean', 'number', 'integer', 'string', 'object']})).toBeTruthy();
+		// expect(val('', {disallow : ['null', 'boolean', 'number', 'integer', 'array', 'object']})).toBeTruthy(); (dojox version does not validate either)
+		expect(val(0.1, {disallow: ['null', 'boolean', 'integer', 'string', 'array', 'object']})).toBeTruthy();
+		expect(val(00, {disallow: ['null', 'boolean', 'string', 'array', 'object']})).toBeTruthy();
+		//expect(val(false, {disallow: ['null', 'number', 'integer', 'string', 'array', 'object']})).toBeTruthy();
+		//expect(val(null, {disallow: ['boolean', 'number', 'integer', 'string', 'array', 'object']})).toBeTruthy();
+
+	});
+
+	// Todo: implement all other test from https://github.com/garycourt/JSV/blob/master/tests/tests3.js
+
+	it('tests the maxDecimal property', function(){
+		var schema = {
+			maxDecimal: 4
+		}
+		expect(val(4.44, schema)).toBeTruthy();
+		expect(val(4.44444, schema)).toBeFalsy();
+	});
+
+	it('tests a more complex schema', function(){
+		var schema = {type: [
+			{ // this one
+				type: 'object',
+				properties: {
+					name: {
+						type: 'string'},
+						id: {type: 'integer'
+					}
+				},
+				additionalProperties: false
+			}, { // or this one
+				type: 'object',
+				properties: {
+					brand: {
+						type: 'string'
+					},
+					id: {
+						type: 'integer'
+					}
+				},
+				additionalProperties: false
+			}
+		]};
+
+		expect(val({name: 'Bill', id: 3}, schema)).toEqual(true);
+		expect(val({brand: 'Moo!', id: 20}, schema)).toEqual(true);
+		expect(val({foo: 'bar'}, schema)).toEqual(false);
+		expect(val({foo: 'bar', brand: 'MooTools', id: 2}, schema)).toEqual(false);
+		expect(val('a string', schema)).toEqual(false);
+
 	});
 
 });
